a0f84e5e1be019dc0b2f8dbb4a89553920fb5c74
Nathan Schneider <nathan.schneider@sap.com>
2018-08-28 17:05:28 -0300

    Start of part 2, test case breaking

--- src/main/day1/day1.ts
+++ src/main/day1/day1.ts
@@ -1,11 +1,30 @@
 export class CircularSequenceDetector {
-    evaluate(input: string): number {
-        let count = 0;
-        for (let i = 0; i < input.length; i++) {
-            if (input[i] === input[(i + 1) % input.length]) {
-                count += parseInt(input[i]);
-            }
-        }
-        return count;
-    }
+
+  constructor(private additionRule: AdditionRule) {}
+
+  evaluate(input: string): number {
+      let count = 0;
+      for (let i = 0; i < input.length; i++) {
+        if (this.additionRule.shouldAdd(i, input)) {
+          count += parseInt(input[i]);
+        } 
+      }
+      return count;
+  }
+}
+
+export interface AdditionRule {
+  shouldAdd(index: number, input: string);
+}
+
+export class PartOneAdditionRule implements AdditionRule {
+  shouldAdd(index: number, input: string) {
+    return input[index] === input[(index + 1) % input.length];
+  }
+}
+
+export class PartTwoAdditionRule implements AdditionRule {
+  shouldAdd(index: number, input: string) {
+    return false;
+  }
 }
--- src/test/day1/day1.spec.ts
+++ src/test/day1/day1.spec.ts
@@ -1,4 +1,4 @@
-import { CircularSequenceDetector } from '../../main/day1/day1';
+import { CircularSequenceDetector, PartOneAdditionRule, PartTwoAdditionRule } from '../../main/day1/day1';
 import { expect } from 'chai';
 import 'mocha';
 
@@ -8,28 +8,42 @@ describe('CircularSequenceDetector', () => {
 
   let csd: CircularSequenceDetector;
 
-  beforeEach(() => {
-    csd = new CircularSequenceDetector();
+  describe('Part 1', () => {
+    beforeEach(() => {
+      csd = new CircularSequenceDetector(new PartOneAdditionRule());
+    });
+
+    it('should return 3 for input 1122', () => {
+      expect(csd.evaluate('1122')).to.equal(3);
+    });
+  
+    it('should return 4 for input 1111', () => {
+      expect(csd.evaluate('1111')).to.equal(4);
+    });
+  
+    it('should return 0 for input 1234', () => {
+      expect(csd.evaluate('1234')).to.equal(0);
+    });
+  
+    it('should return 9 for input 91212129', () => {
+      expect(csd.evaluate('91212129')).to.equal(9);
+    });
+  
+    it('should return 1047 for input of file', () => {
+      const test = readFileSync('src/test/day1/day1.txt', 'utf8');
+      expect(csd.evaluate(test)).to.equal(1047);
+    });
   });
 
-  it('should return 3 for input 1122', () => {
-    expect(csd.evaluate("1122")).to.equal(3);
+  describe('Part 2', () => {
+    beforeEach(() => {
+      csd = new CircularSequenceDetector(new PartTwoAdditionRule());
+    });
+
+    it('should return 6 for input 1212', () => {
+      expect(csd.evaluate('1212')).to.equal(6);
+    });
+  
   });
 
-  it('should return 4 for input 1111', () => {
-    expect(csd.evaluate("1111")).to.equal(4);
-  });
-
-  it('should return 0 for input 1234', () => {
-    expect(csd.evaluate("1234")).to.equal(0);
-  });
-
-  it('should return 9 for input 91212129', () => {
-    expect(csd.evaluate("91212129")).to.equal(9);
-  });
-
-  it('should return 1047 for input of file', () => {
-    const test = readFileSync("src/test/day1/day1.txt", "utf8");
-    expect(csd.evaluate(test)).to.equal(1047);
-  });
 }); 
